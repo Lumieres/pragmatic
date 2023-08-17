71d7181c24957ba30d075b579a358a2550601be4
lumieres <liosho1535@gmail.com>
2023-08-17 21:43:19 +0900

    update setting.py BASE_DIR

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -19,14 +19,15 @@ env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
 )
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 # Set the project base directory
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Take environment variables from .env file
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
