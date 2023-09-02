63711016df5a21f4adf2e0d57bd6843e8bcf481e
lumieres <liosho1535@gmail.com>
2023-09-02 22:43:47 +0900

    django course 24 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 from accountapp.models import HelloWorld
 
@@ -13,23 +13,30 @@ from accountapp.models import HelloWorld
 
 def hello_world(request):
     if request.method == "POST":
-
-        temp = request.POST.get('hello_world_input')
+        temp = request.POST.get("hello_world_input")
 
         new_hello_world = HelloWorld()
         new_hello_world.text = temp
         new_hello_world.save()
 
-        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+        return HttpResponseRedirect(reverse("accountapp:hello_world"))
     else:
         hello_world_list = HelloWorld.objects.all()
         return render(
-            request, "accountapp/hello_world.html", context={"hello_world_list": hello_world_list}
+            request,
+            "accountapp/hello_world.html",
+            context={"hello_world_list": hello_world_list},
         )
 
 
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
-    template_name = 'accountapp/create.html'
+    success_url = reverse_lazy("accountapp:hello_world")
+    template_name = "accountapp/create.html"
+
+
+class AccountDetailView(DetailView):
+    model = User
+    context_object_name = 'target_user'
+    template_name = "accountapp/detail.html"
